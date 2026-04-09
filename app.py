862c8708ab2ba6aa9bf84446a9cc6927fba10379
simolahlb2-gif <simolahlb2@gmail.com>
2026-04-09 15:06:52 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st #⚙️
+st.set_page_config(page_title="Tech Node") #🌐
+st.markdown("<h1 style='text-align:right;'>عقدة التقنية</h1>", unsafe_allow_html=True) #♟️
+st.success("System Online") #✅
